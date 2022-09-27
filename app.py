b88801cc0f6e8355534d68d80ea6df5acc14403f
Hugo Barbosa Rodrigues <hugobrod72@gmail.com>
2022-09-27 11:55:35 -0300

    second commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ number = '1234567890'
 specialCharacters = '!@#$%^&*()|\/'
 
 passLett = lowerCase + upperCase + number + specialCharacters
-passCount = 8
+passCount = 10
 
 password = "".join(random.sample(passLett, passCount))
 print(password)
